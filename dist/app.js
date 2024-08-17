5df57a655410d4c079f55ddc92359bf8e08a6c46
CodeAcademic <oficialjaloliddin@gmai.com>
2024-08-17 14:27:27 +0500

    TS-lesson-1

---
+++ dist/app.js
@@ -0,0 +1,15 @@
+"use strict";
+const students = [
+    {
+        name: "John",
+        score: 56,
+    },
+    {
+        name: "Mark",
+        score: 45,
+    },
+];
+for (const student of students) {
+    //   console.log("Name", student.name);
+    //   console.log("Score", student.score);
+}
---
+++ src/app.ts
@@ -0,0 +1,15 @@
+const students = [
+  {
+    name: "John",
+    score: 56,
+  },
+  {
+    name: "Mark",
+    score: 45,
+  },
+];
+
+for (const student of students) {
+//   console.log("Name", student.name);
+//   console.log("Score", student.score);
+}
